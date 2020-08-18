e235237f669dcc2f9520e4072f5f5103dbe2d56e
Makuss88 <maciborek.grzegorz@gmail.com>
2020-08-18 09:37:50 +0200

    okey

--- backend/timer/models.py
+++ backend/timer/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Timer(models.Model):
 
     times = models.CharField(max_length=20)
+    timer = models.CharField(max_length=8)
+
+    def __str__ (self):
+        return 'ułożenie numer' + self.title + ' to ' + self.timer
